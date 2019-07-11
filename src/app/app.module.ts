a96b76a0b3c87c1bb3b6349e1b6d44d414b7ec84
Pratibha Singh <pratibha010794@gmail.com>
2019-07-11 23:45:03 +0530

    adding files and folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { SearchUserComponent } from './components/search-user/search-user.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    SearchUserComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
